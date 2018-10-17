48c9262d1efae3ae1f1825f1cc61004ff65a8f4d
WeLET-chao <42232582+WeLET-chao@users.noreply.github.com>
2018-10-17 18:56:00 +0800

    11

--- students/ChaoWang/welcomehome.py
+++ students/ChaoWang/welcomehome.py
@@ -6,13 +6,17 @@ mc=Minecraft.create()
 #mc=Minecraft.create("10.163.80.195",4711)
 
 stayed_time=0
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+mc = minecraft.Minecraft.create()
+pos = mc.player.getTilePos()
+mc.setBlock(pos.x+3,pos.y,pos.z,block.STONE,id)
 
 while True:
     print("stay_time"+str(stayed_time))
     time.sleep(0.5)
-    pos=mc.player.getTilePos()
     mc.postToChat("please goto home x=-30 y=-6 z=-40 for 15s to fly")
-    mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z)) 
+    mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z))
     if pos.x==-30 and pos.y==-6 and pos.z==-40:
         mc.postToChat("welcome home")
         stayed_time=stayed_time+1
@@ -21,31 +25,3 @@ while True:
             stayed_time=0
     else:
         stayed_time=0
-        
-     
-=======
-from mcpi.minecraft import Minecraft
-import time
-
-mc=Minecraft.create()
-#mc=Minecraft.create("10.163.80.195",4711)
-
-stayed_time=0
-
-while True:
-    print("stay_time"+str(stayed_time))
-    time.sleep(0.5)
-    pos=mc.player.getTilePos()
-    mc.postToChat("please goto home x=-30 y=-6 z=-40 for 15s to fly")
-    mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z)) 
-    if pos.x==-30 and pos.y==-6 and pos.z==-40:
-        mc.postToChat("welcome home")
-        stayed_time=stayed_time+1
-        if stayed_time>=30:
-            mc.player.setTilePos(-30,10,-40)
-            stayed_time=0
-    else:
-        stayed_time=0
-        
-     
->>>>>>> 08c0e796b45e0378397c7a79aba5e6fd3a005d5a
