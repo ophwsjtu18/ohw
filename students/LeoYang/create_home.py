dd328db2b17f72daac841dbb4fa85822db22fb30
LeoYang <YangLeiSX@sjtu.edu.cn>
2018-10-17 18:55:30 +0800

    update

--- students/LeoYang/create_home.py
+++ students/LeoYang/create_home.py
@@ -4,5 +4,5 @@ import mcpi.block as block
 mc = minecraft.Minecraft.create()
 pos = mc.player.getTilePos()
 
-for i = 1:10
-    mc.setBlock(pos.x+3,pos.y,pos.z+i,block.STONE.id)
+for i in [1,10] :
+    mc.setBlock(pos.x+3,pos.y,pos.z+i/10,block.STONE.id)
