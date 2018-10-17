91ab6fe731fe0a2943bbb520ce977afdc6e06ee7
aweftr <44022811+aweftr@users.noreply.github.com>
2018-10-17 19:35:45 +0800

    asd

--- students/Cheng_Yunlong/welcomehome.py
+++ students/Cheng_Yunlong/welcomehome.py
@@ -7,9 +7,31 @@ mc=Minecraft.create()
 
 stayed_time=0
 pos=mc.player.getTilePos()
-for a in [1,2,3]:
-    for j in [1,2,3]:
-        mc.setBlock (pos.x+3,pos.y+a,pos.z+j,block.STONE.id)
+for a in range(9):
+    for j in range(9):
+        mc.setBlock (pos.x+3+a,pos.y,pos.z+j,89)
+for a in range(4):
+    for j in range(9):
+        mc.setBlock (pos.x+3,pos.y+1+a,pos.z+j,89)
+for a in range(4):
+    for j in range(8):
+        mc.setBlock (pos.x+11,pos.y+a+1,pos.z+j,89)
+for a in range(9):
+    for j in range(9):
+        mc.setBlock (pos.x+3+a,pos.y+5,pos.z+j,88)
+for a in range(5):
+    for j in range(9):
+        mc.setBlock (pos.x+3+j,pos.y+a,pos.z+8,89)
+for a in [1,2,3,4,6,7,8]:
+    for j in range(5):
+        mc.setBlock (pos.x+3+a,pos.y+j,pos.z,89)
+for j in range(2):
+    mc.setBlock (pos.x+3+5,pos.y+j+3,pos.z,89)
+for a in range(2):
+    for j in range(2):
+        mc.setBlock (pos.x+3,pos.y+2+j,pos.z+4+a,20)
+
+
 while True:
     print("stay_time"+str(stayed_time))
     time.sleep(0.5)
