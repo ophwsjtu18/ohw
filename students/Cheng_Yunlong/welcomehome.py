409965543a2290365ea55511d33c9fff61aca8ce
aweftr <44022811+aweftr@users.noreply.github.com>
2018-10-17 18:55:39 +0800

    asd

--- students/Cheng_Yunlong/welcomehome.py
+++ students/Cheng_Yunlong/welcomehome.py
@@ -6,14 +6,16 @@ mc=Minecraft.create()
 #mc=Minecraft.create("10.163.80.195",4711)
 
 stayed_time=0
-
+pos=mc.player.getTilePos()
+for a in [1,2,3]:
+    for j in [1,2,3]:
+        mc.setBlock (pos.x+3,pos.y+a,pos.z+j,block.STONE.id)
 while True:
     print("stay_time"+str(stayed_time))
     time.sleep(0.5)
     pos=mc.player.getTilePos()
     mc.postToChat("please goto home x=-30 y=-6 z=-40 for 15s to fly")
     mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z))
-    mc.setBlock (pos.x+3,pos.y,pos.z,block.STONE.id)
     if pos.x==-30 and pos.y==-6 and pos.z==-40:
         mc.postToChat("welcome home")
         stayed_time=stayed_time+1
