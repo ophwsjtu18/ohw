090ddf96a5df308359559d5635f3316934f14b6b
triplr <hhh2738@sjtu.edu.cn>
2018-11-07 19:56:40 +0800

    .

--- students/QinZijian/set_new_block.py
+++ students/QinZijian/set_new_block.py
@@ -3,4 +3,13 @@ import mcpi.block as block
 
 mc=Minecraft.create()
 pos = mc.player.getTilePos()
-mc.setBlock(pos.x + 3, pos.y, pos.z, 1)
+
+def house(x0, y0, z0, L, W, H, M):
+    for x in range(L):
+        mc.setBlock(x0 + x, y0, z0, M)
+    for y in range(H):
+        mc.setBlock(x0, y0 + y, z0, M)
+    for z in range(W):
+        mc.setBlock(x0, y0, z0 + z, M)
+
+house(40, 20, 50, 10, 10, 6, 1)
