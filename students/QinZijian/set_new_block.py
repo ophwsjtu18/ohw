a4e6fc8d7f331ce0931bcef1fa1024c653888f84
triplr <hhh2738@sjtu.edu.cn>
2018-11-07 20:10:09 +0800

    .

--- students/QinZijian/set_new_block.py
+++ students/QinZijian/set_new_block.py
@@ -6,10 +6,15 @@ pos = mc.player.getTilePos()
 
 def house(x0, y0, z0, L, W, H, M):
     for x in range(L):
-        mc.setBlock(x0 + x, y0, z0, M)
-    for y in range(H):
-        mc.setBlock(x0, y0 + y, z0, M)
-    for z in range(W):
-        mc.setBlock(x0, y0, z0 + z, M)
+        for y in range(H):
+            for z in range(W):
+                mc.setBlock(x0 + x, y0 + y, z0 + z, M)
 
-house(40, 20, 50, 10, 10, 6, 1)
+    for x in range(L - 2):
+        for y in range(H - 2):
+            for z in range(W - 2):
+                mc.setBlock(x0 + x + 1, y0 + y + 1, z0 + z + 1, 0)
+while True:
+    mc.postToChat("x: " + str(pos.x) + "y: " + str(pos.y) + "z: " + str(pos.z))
+
+house(pos.x, pos.y, pos.z, 3, 3, 3, 89)
