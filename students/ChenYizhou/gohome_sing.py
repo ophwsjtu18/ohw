39c135cc4e708fbcd84789d8335f9ed51dee574e
SJTUzhou <1193106707+SJTUzhou@users.noreply.github.com>
2018-11-07 22:27:13 +0800

    1

--- students/ChenYizhou/gohome_sing.py
+++ students/ChenYizhou/gohome_sing.py
@@ -19,7 +19,7 @@ def fly_sing_loop(mc):
         pos=mc.player.getTilePos()
         mc.postToChat("please go to home x=86 y=22 z=111 for 15s to fly")
         mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z))
-
+        '''
         if pos.x==home_position[0] and pos.y==home_position[1] and pos.z==home_position[2]:
             mc.postToChat("welcome home")
             stayed_time = stayed_time + 1
@@ -33,6 +33,27 @@ def fly_sing_loop(mc):
                 count_fly = count_fly + 1
                 # get back to home from the sky
                 mc.player.setTilePos(home_position[0],home_position[1],home_position[2])
+        else:
+            stayed_time = 0
+        '''
+        if pos.x==home_position[0] and pos.y==home_position[1] and pos.z==home_position[2]:
+            mc.postToChat("welcome home")
+            stayed_time = stayed_time + 1
+            resp = ser.readline()
+            rs = str(resp)
+            if 'ON' in rs:
+                print("got ON")
+                # fly above
+                mc.player.setTilePos(home_position[0], home_position[1] + 30, home_position[2])
+                stayed_time = 0
+                # play a song
+                song_name, song_content = list(song_dict.items())[count_fly % 3]
+                my_song_player.play_single_song(ser, song_name, song_content)
+                count_fly = count_fly + 1
+                # get back to home from the sky
+                mc.player.setTilePos(home_position[0], home_position[1], home_position[2])
+            if 'OFF' in rs:
+                print("got OFF")
         else:
             stayed_time=0
 
