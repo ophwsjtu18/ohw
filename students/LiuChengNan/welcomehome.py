678f73a5e98907fb6b8f3cbdb14256cc9a5744c2
liunolan <34544910+liunolan@users.noreply.github.com>
2018-11-14 14:07:01 +0800

    14点06分

--- students/LiuChengNan/welcomehome.py
+++ students/LiuChengNan/welcomehome.py
@@ -1,4 +1,10 @@
 # coding:utf-8
+"""
+将完成以下功能：
+识别电子积木，并完成串口通信（唱歌，按钮飞天）。
+MC中，回家提示，回家跳跃，回家播放音乐，音乐按跳跃次数不同变化。
+house类建房子。
+"""
 #导入我的世界包 及 串口通信包
 from mcpi.minecraft import Minecraft
 import time
@@ -34,28 +40,38 @@ rows = open("songs.csv").read().split('\n')
 songs = {}
 for row in rows:
     songs[row.split(',')[0]]=row.split(',')[1:]
-for key, value in songs:
-    print(key,value)
 
-action = "empty"
+
+def jump():
+    mc.player.setTilePos(pos.x, pos.y+3, pos.z)
+
+def music(times):
+    choice = times % 3
+    name = songs.keys()[choice]
+    for i in songs[name]:
+        ser.write(i.encode())
+        ser.write('a'.encode())
+
+
 
 while True:
+    #提示回家
     print("stay_time"+str(stayed_time))
     time.sleep(0.5)
     pos=mc.player.getTilePos()
-    mc.postToChat("please goto home -193<=x<=-180 y=25 112<=z<=122 for 5s to fly")
+    mc.postToChat("please go back home -193<=x<=-180 y=25 112<=z<=122 for 5s to fly")
     mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z))
-    if -193<=pos.x<=-180 and pos.y==25 and 112<=pos.z<=122:
-        mc.postToChat("welcome home")
-        stayed_time=stayed_time+1
-        if stayed_time>=5:
-            mc.player.setTilePos(pos.x, pos.y+3, pos.z)
-            stayed_time=0
 
-            while True:
-                for i in lyric[1]:
-                   ser.write(i.encode())
-                   ser.write('a'.encode())
-                break
+    times = 0 #起跳次数
+
+    if -193 <= pos.x <= -180 and pos.y== 25 and 112 <= pos.z <= 122:
+        mc.postToChat("welcome home")
+        stayed_time = stayed_time + 1
+        if stayed_time >= 5:
+            jump()
+            music(times)
+            stayed_time = 0
+            times += 1
+
     else:
-        stayed_time=0
+        stayed_time = 0
