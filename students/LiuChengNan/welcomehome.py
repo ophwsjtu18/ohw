ea1ab1ba4735e6b5ed08cc33d52a623af29163c2
liunolan <34544910+liunolan@users.noreply.github.com>
2018-11-14 13:18:50 +0800

    add code

--- students/LiuChengNan/welcomehome.py
+++ students/LiuChengNan/welcomehome.py
@@ -1,38 +1,61 @@
+# coding:utf-8
+#导入我的世界包 及 串口通信包
 from mcpi.minecraft import Minecraft
-import mcpi.block as block
+import time
+import serial
+import serial.tools.list_ports
 import time
 
-mc=Minecraft.create()
+#匹配单片机串口
+print('hello')
+ports = list(serial.tools.list_ports.comports())
+print(ports)
+
+mc=Minecraft.create() #连接bukkit 服务器
 #mc=Minecraft.create("10.163.80.195",4711)
 
 stayed_time=0
-pos=mc.player.getTilePos()
-for i in range(10):
-    for j in range(10):
-        mc.setBlock(pos.x+i, pos.y, pos.z+j, block.WOOD.id)
-        mc.setBlock(pos.x+i, pos.y+9, pos.z+j, block.STONE.id)
-for i in range(10):
-    for j in range(10):
-        mc.setBlock(pos.x+i, pos.y+j, pos.z, block.WOOD.id)
-        mc.setBlock(pos.x+i, pos.y+j, pos.z+9, block.WOOD.id)
-for i in range(10):
-    for j in range(10):
-        mc.setBlock(pos.x, pos.y+i, pos.z+j, block.WOOD.id)
-        mc.setBlock(pos.x+9, pos.y+i, pos.z+j, block.WOOD.id)
+
+for p in ports:
+    print (p[1])
+    if "SERIAL" in p[1] or "UART" in p[1]:
+        ser = serial.Serial(port=p[0])
+    else:
+        print ("No Arduino Device was found connected to the computer")
+
+#ser=serial.Serial(port='COM4')
+#ser=serial.Serial(port='/dev/ttymodem542')
+#wait 2 seconds for arduino board restart
+time.sleep(2)
 
 
+#读入歌曲；转为字典
+rows = open("songs.csv").read().split('\n')
+songs = {}
+for row in rows:
+    songs[row.split(',')[0]]=row.split(',')[1:]
+for key, value in songs:
+    print(key,value)
+
+action = "empty"
 
 while True:
     print("stay_time"+str(stayed_time))
     time.sleep(0.5)
     pos=mc.player.getTilePos()
-    mc.postToChat("please goto home -3<=x<=0 35<=y<=37 -10<=z-7 for 5s to fly")
+    mc.postToChat("please goto home -193<=x<=-180 y=25 112<=z<=122 for 5s to fly")
     mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z))
-    if -3<=pos.x<=0 and 35<=pos.y<=37 and -10<=pos.z<=-7:
+    if -193<=pos.x<=-180 and pos.y==25 and 112<=pos.z<=122:
         mc.postToChat("welcome home")
         stayed_time=stayed_time+1
         if stayed_time>=5:
             mc.player.setTilePos(pos.x, pos.y+3, pos.z)
             stayed_time=0
+
+            while True:
+                for i in lyric[1]:
+                   ser.write(i.encode())
+                   ser.write('a'.encode())
+                break
     else:
         stayed_time=0
