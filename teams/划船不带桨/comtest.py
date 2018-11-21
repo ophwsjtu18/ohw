d9eb72f43ea6a01dd2bbcb009da02e75dfcde9fc
triplr <hhh2738@sjtu.edu.cn>
2018-11-21 19:16:35 +0800

    change

--- teams/划船不带桨/comtest.py
+++ teams/划船不带桨/comtest.py
@@ -9,7 +9,7 @@ print (ports)
 
 for p in ports:
     print (p[1])
-    if "USB-SERIAL" in p[1]:
+    if "Arduino Uno" in p[1]:
 	    ser=serial.Serial(port=p[0],baudrate=9600)
     else :
 	    print ("No Arduino Device was found connected to the computer")
