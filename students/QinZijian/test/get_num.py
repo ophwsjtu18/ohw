8840adf6ff822f311a98197d1733df0037b0d7b1
triplr <hhh2738@sjtu.edu.cn>
2018-12-12 19:34:39 +0800

    更新了备注

--- students/QinZijian/test/get_num.py
+++ students/QinZijian/test/get_num.py
@@ -1,4 +1,6 @@
 from urllib import request
-response = request.urlopen('http://192.168.142.132/person_num.html')
+response = request.urlopen('http://10.163.126.35/person_num.html') 
+#桥接模式设置虚拟机，复制物理网络连接状态，ipconfig，填入ip地址
+
 page = response.read().decode('utf-8')
 print(page)
