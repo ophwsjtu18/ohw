c2648627a20665397fe4771de170fe70088479d9
triplr <hhh2738@sjtu.edu.cn>
2018-12-12 22:20:07 +0800

    。

--- students/QinZijian/test/get_num.py
+++ students/QinZijian/test/get_num.py
@@ -1,6 +1,9 @@
+import time
 from urllib import request
-response = request.urlopen('http://10.163.126.35/person_num.html') 
-#桥接模式设置虚拟机，复制物理网络连接状态，ipconfig，填入ip地址
+while True:
+    response = request.urlopen('http://192.168.1.101/cmd.html') 
+    #桥接模式设置虚拟机，复制物理网络连接状态，ipconfig，填入ip地址
 
-page = response.read().decode('utf-8')
-print(page)
+    page = response.read().decode('utf-8')
+    print(page)
+    time.sleep(0.2)
