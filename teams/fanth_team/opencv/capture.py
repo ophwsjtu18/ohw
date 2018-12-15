874317c5313aed70113e18b86a4d646f964e3c4a
aweftr <44022811+aweftr@users.noreply.github.com>
2018-12-15 23:00:41 +0800

    change ip

--- teams/fanth_team/opencv/capture.py
+++ teams/fanth_team/opencv/capture.py
@@ -1,7 +1,7 @@
 from urllib import request
 import time
 while(True):
-	f = request.urlopen("http://192.168.209.138/data.html")
+	f = request.urlopen("http://192.168.137.229/data.html")
 	data = f.read()
 	data = data.decode('utf-8')
 	data = data.split('\n')
