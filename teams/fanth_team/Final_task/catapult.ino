a6a2d070216138ed6194fd96e3dd136569a2201f
WeLET-chao <42232582+WeLET-chao@users.noreply.github.com>
2018-12-15 20:06:04 +0800

    减少参数

--- teams/fanth_team/Final_task/catapult.ino
+++ teams/fanth_team/Final_task/catapult.ino
@@ -4,7 +4,7 @@ Servo armServo ;
 Servo trayServo ;
 Servo baseServo ;
 
-#define lockOpen    0
+#define lockOpen    180
 #define lockClosed  90
 
 #define armOpen     180
@@ -31,21 +31,21 @@ void sweep ( Servo servo , int from , int to , int step_time  ) {
 }
 
 void rest() {
-    sweep(lockServo,lockServo.read(),lockOpen,10) ;
     sweep(armServo,armServo.read(),armOpen,5) ;
+    sweep(lockServo,lockServo.read(),lockClosed,10) ;
     sweep(trayServo,trayServo.read(),trayClosed,10);
     sweep(baseServo,baseServo.read(),basePoint,10);
     }
 
 void shootprep(int strength) {
+    sweep(trayServo,trayServo.read(),trayOpened,10) ;
+    delay(1000);
+    sweep(trayServo,trayServo.read(),trayClosed,10) ;
     if(strength<0)  strength = 0;
     if(strength>30) strength = 30;
     sweep(lockServo,lockServo.read(),lockClosed,10);
     sweep(armServo,armServo.read(),180-3*strength,5);
     delay(100);
-    sweep(trayServo,trayServo.read(),trayOpened,10) ;
-    delay(1000);
-    sweep(trayServo,trayServo.read(),trayClosed,10) ;
     }
 void shoot(){
     sweep(lockServo,lockServo.read(),lockOpen,10);
@@ -53,10 +53,12 @@ void shoot(){
     sweep(armServo,armServo.read(),armOpen,5) ;
     }
 
-void auto_run(int angle,int prep,int strength,int fire){
+void auto_run(int angle,int strength){
     sweep(baseServo,baseServo.read(),basePoint+angle,15);
-    if(prep|fire)   shootprep(strength);
-    if(fire)        shoot();
+    if(strength) {
+        shootprep(strength);
+        shoot();
+        }
     }
 
 
@@ -68,13 +70,13 @@ void setup() {
     lockServo.attach(3);
     armServo.attach(5) ;
     trayServo.attach(6);
-    baseServo.attach(9) ; 
+    baseServo.attach(9) ;
     rest();
 }
 
 
 void loop(){
-    int cmd[]={0, 0, 0, 0};
+    int cmd[]={0, 0};
     int cur;
     String item;
     int itemint;
@@ -89,5 +91,5 @@ void loop(){
           cmd[(int)(cur - 1)] = itemint;
           }
        }
-    auto_run(cmd[0],cmd[1],cmd[2],cmd[3]);
-    }
+    auto_run(cmd[0],cmd[1]);
+    }  
