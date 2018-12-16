3dba8c8761841d09eb45fd56837157c08c27e9fa
Gsh1707 <39880138+Gsh1707@users.noreply.github.com>
2018-12-16 18:46:08 +0800

    1

--- teams/划船不带桨/running_cmd.py
+++ teams/划船不带桨/running_cmd.py
@@ -54,7 +54,7 @@ while True:
         page = response.read()
         page = page.decode('utf-8')
         raw_angle = page.split(',')[1]
-        base_angle = str(raw_angle/6)
+        base_angle = str(float(raw_angle)/6)
         arm_angle = '20'
         shoot_cmd = "A " + base_angle + " " + arm_angle + " 120 0 0 "
         ser.write(shoot_cmd.encode())
