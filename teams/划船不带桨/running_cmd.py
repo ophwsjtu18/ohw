4899505123e343978169086be4c2079e1e50fc75
SJTUzhou <1193106707+SJTUzhou@users.noreply.github.com>
2018-12-16 23:53:47 +0800

    1

--- teams/划船不带桨/running_cmd.py
+++ teams/划船不带桨/running_cmd.py
@@ -5,7 +5,6 @@ import serial.tools.list_ports
 from urllib import request
 
 
-<<<<<<< HEAD
 def get_cmd_info(ip="192.168.199.213"):
     response = request.urlopen("http://" + ip + "/cmd.html")
     page = response.read()
@@ -59,9 +58,9 @@ def catapult_shoot(ser):
 def check_points_per_step(mc):
     # check get or lose points every single step.
     # change block id here!
-    good_id = 42
-    bad_id = 75
-    normal_id = 24
+    good_id = 35
+    bad_id = 1
+    normal_id = 2
     pos = mc.player.getTilePos()
     point_per_step = 0
     if mc.getBlock(pos.x, pos.y-1, pos.z) == good_id:
@@ -128,62 +127,3 @@ def main():
 
 
 main()
-
-=======
-mc=Minecraft.create()
-ports = list(serial.tools.list_ports.comports())
-print(ports)
-for p in ports:
-    print(p[1])
-    if "Arduino Uno" in p[1]:
-        ser = serial.Serial(port=p[0], baudrate=9600)
-    else:
-        print("No Arduino Device was found connected to the computer")
-
-while True:
-    instruct = input("a: controlled by camera.  b: shoot a stone")
-    if instruct == 'a':
-        start = time.time()
-        while True:
-            pos=mc.player.getTilePos()
-            response = request.urlopen('http://192.168.199.213/cmd.html')
-            page = response.read()
-            page = page.decode('utf-8')
-            pos=mc.player.getTilePos()
-            print(page)
-            pos_cmd = page.split(',')[0]
-            time.sleep(1)
-            if (time.time()-start) > 60:
-                break
-            if(pos_cmd=='forward'):
-                if(mc.getBlock(pos.x,pos.y,pos.z+1)==0):
-                    print("move forward")
-                    mc.postToChat("move forward")
-                    mc.player.setTilePos(pos.x,pos.y,pos.z+1)
-            elif(pos_cmd=='back'):
-                print("move backward")
-                mc.postToChat("move backward")
-                mc.player.setTilePos(pos.x,pos.y,pos.z-1)
-            elif(pos_cmd=='left'):
-                if(mc.getBlock(pos.x+1,pos.y,pos.z)==0):
-                    print("move left")
-                    mc.postToChat("move left")
-                    mc.player.setTilePos(pos.x+1,pos.y,pos.z)
-            elif(pos_cmd=='right'):
-                if(mc.getBlock(pos.x-1,pos.y,pos.z)==0):
-                    print("move right")
-                    mc.postToChat("move right")
-                    mc.player.setTilePos(pos.x-1,pos.y,pos.z)
-    elif instruct == 'b':
-        response = request.urlopen('http://192.168.199.213/cmd.html')
-        page = response.read()
-        page = page.decode('utf-8')
-        raw_angle = page.split(',')[1]
-        base_angle = str(float(raw_angle)/6)
-        arm_angle = '20'
-        shoot_cmd = "A " + base_angle + " " + arm_angle + " 120 0 0 "
-        ser.write(shoot_cmd.encode())
-        print(shoot_cmd)
-        print("Waiting for execution......")
-        time.sleep(20)
->>>>>>> 10dc96b35e224afa50cc47bee8a4a4c1f7dda352
